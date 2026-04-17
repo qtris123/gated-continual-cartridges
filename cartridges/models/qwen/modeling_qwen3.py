75dd307bba7824f6566acd68d6111b67b9e2d04d
qtris123 <voquangtri2021@gmail.com>
2026-04-17 17:46:52 -0400

    upgrade to transformers 5.5.4 requires this

--- cartridges/models/qwen/modeling_qwen3.py
+++ cartridges/models/qwen/modeling_qwen3.py
@@ -392,7 +392,7 @@ class FlexQwen3Model(FlexQwen3PreTrainedModel):
 
 @auto_docstring
 class FlexQwen3ForCausalLM(FlexQwen3PreTrainedModel, GenerationMixin):
-    _tied_weights_keys = ["lm_head.weight"]
+    _tied_weights_keys = {"lm_head.weight": "model.embed_tokens.weight"}
     _tp_plan = {"lm_head": "colwise_rep"}
     _pp_plan = {"lm_head": (["hidden_states"], ["logits"])}
 
